6f8eb974884210f55d9371b1d0375f0bb75a424a
Alexander01998 <contact.wurstimperium@gmail.com>
2016-04-21 16:06:18 +0200

    Change version to 3.0

--- Wurst Client for MC 1.9.X/src/tk/wurst_client/WurstClient.java
+++ Wurst Client for MC 1.9.X/src/tk/wurst_client/WurstClient.java
@@ -29,7 +29,7 @@ public enum WurstClient
 {
 	INSTANCE;
 	
-	public static final String VERSION = "3.0pre4";
+	public static final String VERSION = "3.0";
 	public boolean startupMessageDisabled = false;
 	
 	public AnalyticsManager analytics;
