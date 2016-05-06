bf9e9ed1df6d4932ad8ea13acf8dc8e0b9ef8460
MCCCS <mcccs@users.noreply.github.com>
2016-05-06 17:41:15 +0300

    Create MassMSGCmd.java

---
+++ Wurst Client for MC 1.9.X/src/tk/wurst_client/commands/MassMSGCmd.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2014 - 2016 | Wurst-Imperium | All rights reserved.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package tk.wurst_client.commands;
+
+import tk.wurst_client.commands.Cmd.Info;
+import net.minecraft.network.play.client.CPacketChatMessage;
+
+@Info(help = "Allows you to spam other players text.",
+	name = "massmsg",
+	syntax = {"/<command> <message>"},
+public class MassTpaMod extends Cmd implements UpdateListener,
+	ChatInputListener
+{
+	@Override
+	public void execute(String[] args) throws Error
+	{
+		if(!wurst.mods.massMsgCmdMod.isEnabled())
+			wurst.mods.massMsgCmdMod.setEnabled(true);
+			
+			
+			if(args.length > 1)
+		{
+			String message = "½$" + args[0];
+			for(int i = 1; i < args.length; i++)
+				message += " " + args[i];
+			mc.thePlayer.sendQueue.addToSendQueue(new CPacketChatMessage(
+				message));
+		}
+			
+		
+		}
+}
