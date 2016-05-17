66f8abef972d28b66289e795cc98d718082de4f3
Alexander01998 <contact.wurstimperium@gmail.com>
2016-05-17 13:41:49 +0200

    Add LogSpammer to ModManager

--- Wurst Client for MC 1.9.X/src/tk/wurst_client/mods/ModManager.java
+++ Wurst Client for MC 1.9.X/src/tk/wurst_client/mods/ModManager.java
@@ -97,6 +97,7 @@ public class ModManager
 	public final KillauraMod killauraMod = new KillauraMod();
 	public final KillerPotionMod killerPotionMod = new KillerPotionMod();
 	public final LiquidsMod liquidsMod = new LiquidsMod();
+	public final LogSpammerMod logSpammerMod = new LogSpammerMod();
 	public final LsdMod lsdMod = new LsdMod();
 	public final MassTpaMod massTpaMod = new MassTpaMod();
 	public final MenuWalkMod menuWalkMod = new MenuWalkMod();
