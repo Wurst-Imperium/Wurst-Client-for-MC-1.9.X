46025dd15ac729ec26070427537411401a6853db
Alexander01998 <contact.wurstimperium@gmail.com>
2016-05-25 13:06:55 +0200

    Implement YesCheat+ modes

--- Wurst Client for MC 1.9.X/src/tk/wurst_client/mods/YesCheatMod.java
+++ Wurst Client for MC 1.9.X/src/tk/wurst_client/mods/YesCheatMod.java
@@ -11,30 +11,51 @@ import java.util.HashSet;
 
 import tk.wurst_client.mods.Mod.Category;
 import tk.wurst_client.mods.Mod.Info;
+import tk.wurst_client.navigator.settings.ModeSetting;
 
 @Info(category = Category.MISC,
-	description = "Makes other mods bypass NoCheat+ or blocks them if\n"
+	description = "Makes other mods bypass AntiCheat plugins or blocks them if\n"
 		+ "they can't.",
 	name = "YesCheat+",
-	tags = "YesCheatPlus, NoCheat+, NoCheatPlus, yes cheat plus, no cheat plus, ncp bypasses",
+	tags = "YesCheatPlus, NoCheat+, NoCheatPlus, AntiMAC, yes cheat plus, no cheat plus, anti mac, ncp bypasses",
 	help = "Mods/YesCheat")
 public class YesCheatMod extends Mod
 {
-	private HashSet<Mod> blockedMods;
+	private final HashSet<Mod> blockedMods = new HashSet<Mod>();
+	private int bypassLevel = 2;
+	
+	@Override
+	public void initSettings()
+	{
+		settings.add(new ModeSetting("Bypass Level", BypassLevel.getNames(),
+			bypassLevel)
+		{
+			@Override
+			public void update()
+			{
+				bypassLevel = getSelected();
+				
+				if(isActive())
+					onDisable();
+				blockedMods.clear();
+				if(isActive())
+					onEnable();
+			}
+		});
+	}
 	
 	@Override
 	public void onEnable()
 	{
+		// TODO: Remove this
 		if(wurst.mods.antiMacMod.isEnabled())
 			wurst.mods.antiMacMod.setEnabled(false);
-		if(blockedMods == null)
-		{
-			blockedMods = new HashSet<>();
+		
+		if(blockedMods.isEmpty())
 			for(Mod mod : wurst.mods.getAllMods())
-				if(!mod.getClass().getAnnotation(Mod.Info.class)
-					.noCheatCompatible())
+				if(!getBypassLevel().doesBypass(mod.getBypasses()))
 					blockedMods.add(mod);
-		}
+		
 		for(Mod mod : blockedMods)
 			mod.setBlocked(true);
 	}
@@ -44,5 +65,46 @@ public class YesCheatMod extends Mod
 	{
 		for(Mod mod : blockedMods)
 			mod.setBlocked(false);
+	}
+	
+	public BypassLevel getBypassLevel()
+	{
+		return BypassLevel.values()[bypassLevel];
+	}
+	
+	private interface BypassTest
+	{
+		public boolean doesBypass(Bypasses b);
+	}
+	
+	public enum BypassLevel
+	{
+		MINEPLEX_ANTICHEAT("Mineplex AntiCheat", (b) -> b.mineplexAntiCheat()),
+		ANTICHEAT("AntiCheat", (b) -> b.antiCheat()),
+		OLDER_NCP("Older NoCheat+", (b) -> b.olderNCP()),
+		LATEST_NCP("Latest NoCheat+", (b) -> b.latestNCP()),
+		GHOST_MODE("Ghost Mode", (b) -> b.ghostMode());
+		
+		private final String name;
+		private final BypassTest test;
+		
+		private BypassLevel(String name, BypassTest test)
+		{
+			this.name = name;
+			this.test = test;
+		}
+		
+		public static String[] getNames()
+		{
+			String[] names = new String[values().length];
+			for(int i = 0; i < names.length; i++)
+				names[i] = values()[i].name;
+			return names;
+		}
+		
+		public boolean doesBypass(Bypasses bypasses)
+		{
+			return test.doesBypass(bypasses);
+		}
 	}
 }
