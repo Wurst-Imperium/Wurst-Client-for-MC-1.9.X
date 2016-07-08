fe6294d090989f8d78e6e20166a18c58ccf4c00c
Marco MC <Marco.MC0902@gmail.com>
2016-07-08 16:20:44 +0200

    Better Step
    The Packet Step is now very better.

--- Wurst Client for MC 1.9.X/src/tk/wurst_client/mods/StepMod.java
+++ Wurst Client for MC 1.9.X/src/tk/wurst_client/mods/StepMod.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2014 - 2016 | Wurst-Imperium | All rights reserved.
+ * Copyright Â© 2014 - 2016 | Wurst-Imperium | All rights reserved.
  * 
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -66,7 +66,7 @@ public class StepMod extends Mod implements UpdateListener
 			.ordinal())
 		{
 			mc.thePlayer.stepHeight = 0.5F;
-			if(mc.thePlayer.isCollidedHorizontally && mc.thePlayer.onGround)
+			if(mc.thePlayer.onGround && !mc.thePlayer.isOnLadder() && (mc.thePlayer.movementInput.moveForward != 0.0F || mc.thePlayer.movementInput.moveStrafe != 0.0F) && canStep() && !mc.thePlayer.movementInput.jump && mc.thePlayer.isCollidedHorizontally)
 			{
 				mc.getNetHandler().addToSendQueue(
 					new CPacketPlayer.C04PacketPlayerPosition(
@@ -111,5 +111,58 @@ public class StepMod extends Mod implements UpdateListener
 				mode.lock(0);
 				break;
 		}
+	}
+	
+	public Block getBlock(AxisAlignedBB bb)
+	{
+		int y = (int)bb.minY;
+		for(int x = MathHelper.floor_double(bb.minX); x < MathHelper.floor_double(bb.maxX) + 1; x++) {
+			for(int z = MathHelper.floor_double(bb.minZ); z < MathHelper.floor_double(bb.maxZ) + 1; z++) {
+				Block block = mc.theWorld.getBlockState(new BlockPos(x, y, z)).getBlock();
+				if(block != null) 
+					return block;
+			}
+		}
+		return null;
+	}
+	
+	public boolean canStep()
+	{
+		ArrayList<BlockPos> collisionBlocks = new ArrayList();
+		
+		EntityPlayer p = mc.thePlayer;
+		BlockPos var1 = new BlockPos(p.getEntityBoundingBox().minX - 0.001D, p.getEntityBoundingBox().minY - 0.001D, p.getEntityBoundingBox().minZ - 0.001D);
+		BlockPos var2 = new BlockPos(p.getEntityBoundingBox().maxX + 0.001D, p.getEntityBoundingBox().maxY + 0.001D, p.getEntityBoundingBox().maxZ + 0.001D);
+		int var4;
+		if(p.worldObj.isAreaLoaded(var1, var2)) {
+			for(int var3 = var1.getX(); var3 <= var2.getX(); var3++) {
+				for(var4 = var1.getY(); var4 <= var2.getY(); var4++) {
+					for(int var5 = var1.getZ(); var5 <= var2.getZ(); var5++) {
+						BlockPos blockPos = new BlockPos(var3, var4, var5);
+						IBlockState var7 = p.worldObj.getBlockState(blockPos);
+						try{
+							if((var4 > p.posY - 1.0D) && (var4 <= p.posY))
+								collisionBlocks.add(blockPos);
+						}catch(Throwable throwable) { }
+					}
+				}
+			}
+		}
+		
+		for(BlockPos pos : collisionBlocks) {
+			if(!(p.worldObj.getBlockState(pos.add(0, 1, 0)).getBlock() instanceof BlockFenceGate))
+				if(p.worldObj.getBlockState(pos.add(0, 1, 0)).getBlock().getCollisionBoundingBox(mc.theWorld.getBlockState(pos), mc.theWorld, new BlockPos(p.posX, p.posY - 1.0D, p.posZ)) != null)
+					return false;
+			
+			if(!(p.worldObj.getBlockState(pos.add(0, 1, 0)).getBlock() instanceof BlockFenceGate))
+				if(p.worldObj.getBlockState(pos.add(0, 1, 0)).getBlock().getCollisionBoundingBox(mc.theWorld.getBlockState(pos), mc.theWorld, new BlockPos(p.posX, p.posY - 1.0D, p.posZ)) != null)
+					return false;
+		}
+		return true;
+	}
+	
+	public Block getBlock(double offset)
+	{
+		return getBlock(mc.thePlayer.getEntityBoundingBox().offset(0.0D, offset, 0.0D));
 	}
 }
