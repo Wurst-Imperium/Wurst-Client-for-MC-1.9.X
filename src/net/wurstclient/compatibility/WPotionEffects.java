949d81f0802390ca0739340f9cb39651c9a0017e
Alexander01998 <contact.wurstimperium@gmail.com>
2017-05-01 19:42:45 +0200

    Add WPotionEffects

---
+++ src/net/wurstclient/compatibility/WPotionEffects.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2014 - 2017 | Wurst-Imperium | All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package net.wurstclient.compatibility;
+
+import net.minecraft.init.MobEffects;
+import net.minecraft.potion.Potion;
+
+public class WPotionEffects
+{
+	public static final Potion SPEED = MobEffects.moveSpeed;
+	public static final Potion SLOWNESS = MobEffects.moveSlowdown;
+	public static final Potion HASTE = MobEffects.digSpeed;
+	public static final Potion MINING_FATIGUE = MobEffects.digSlowdown;
+	public static final Potion STRENGTH = MobEffects.damageBoost;
+	public static final Potion INSTANT_HEALTH = MobEffects.heal;
+	public static final Potion INSTANT_DAMAGE = MobEffects.harm;
+	public static final Potion JUMP_BOOST = MobEffects.jump;
+	public static final Potion NAUSEA = MobEffects.confusion;
+	public static final Potion REGENERATION = MobEffects.regeneration;
+	public static final Potion RESISTANCE = MobEffects.resistance;
+	public static final Potion FIRE_RESISTANCE = MobEffects.fireResistance;
+	public static final Potion WATER_BREATHING = MobEffects.waterBreathing;
+	public static final Potion INVISIBILITY = MobEffects.invisibility;
+	public static final Potion BLINDNESS = MobEffects.blindness;
+	public static final Potion NIGHT_VISION = MobEffects.nightVision;
+	public static final Potion HUNGER = MobEffects.hunger;
+	public static final Potion WEAKNESS = MobEffects.weakness;
+	public static final Potion POISON = MobEffects.poison;
+	public static final Potion WITHER = MobEffects.wither;
+	public static final Potion HEALTH_BOOST = MobEffects.healthBoost;
+	public static final Potion ABSORPTION = MobEffects.absorption;
+	public static final Potion SATURATION = MobEffects.saturation;
+}
