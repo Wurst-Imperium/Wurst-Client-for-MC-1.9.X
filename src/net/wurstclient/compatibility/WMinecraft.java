51eaf4e9f64490bfd0dff2d7e399f81fc128058e
Alexander01998 <contact.wurstimperium@gmail.com>
2017-05-03 08:40:00 +0200

    Update GuiCleanUp

--- src/net/wurstclient/compatibility/WMinecraft.java
+++ src/net/wurstclient/compatibility/WMinecraft.java
@@ -1,5 +1,9 @@
 package net.wurstclient.compatibility;
 
+import java.util.Collections;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.multiplayer.WorldClient;
@@ -11,6 +15,17 @@ public final class WMinecraft
 	public static final String DISPLAY_VERSION = "1.9";
 	public static final boolean REALMS = false;
 	public static final boolean COOLDOWN = true;
+	
+	public static final NavigableMap<Integer, String> PROTOCOLS;
+	static
+	{
+		TreeMap<Integer, String> protocols = new TreeMap<>();
+		protocols.put(107, "1.9");
+		protocols.put(108, "1.9.1");
+		protocols.put(109, "1.9.2");
+		protocols.put(110, "1.9.3/4");
+		PROTOCOLS = Collections.unmodifiableNavigableMap(protocols);
+	}
 	
 	private static final Minecraft mc = Minecraft.getMinecraft();
 	
