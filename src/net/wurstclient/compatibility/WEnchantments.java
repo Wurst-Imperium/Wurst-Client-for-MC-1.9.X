276b747addc1061b48f24087605f2b359098ee17
Alexander01998 <contact.wurstimperium@gmail.com>
2018-04-16 14:23:00 +0200

    Improve AutoArmorMod

--- src/net/wurstclient/compatibility/WEnchantments.java
+++ src/net/wurstclient/compatibility/WEnchantments.java
@@ -14,6 +14,7 @@ import net.minecraft.item.ItemStack;
 
 public final class WEnchantments
 {
+	public static final Enchantment PROTECTION = Enchantments.protection;
 	public static final Enchantment EFFICIENCY = Enchantments.efficiency;
 	public static final Enchantment SILK_TOUCH = Enchantments.silkTouch;
 	
