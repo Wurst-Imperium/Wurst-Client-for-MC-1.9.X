ce7647d822df84523d51ece4e9c35c01911c4fe6
Alexander01998 <contact.wurstimperium@gmail.com>
2018-08-06 13:36:00 +0200

    Add nether warts to AutoFarm

--- src/net/wurstclient/compatibility/WBlock.java
+++ src/net/wurstclient/compatibility/WBlock.java
@@ -10,6 +10,7 @@ package net.wurstclient.compatibility;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockChest;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
@@ -42,6 +43,12 @@ public final class WBlock
 	public static Material getMaterial(BlockPos pos)
 	{
 		return getState(pos).getMaterial();
+	}
+	
+	public static int getIntegerProperty(IBlockState state,
+		PropertyInteger prop)
+	{
+		return state.getValue(prop);
 	}
 	
 	public static AxisAlignedBB getBoundingBox(BlockPos pos)
